9619b9c7ac4e78cb888d98d743d265ad63996b0c
Tomasz Bałdyga <tomaszbaldyga3@gmail.com>
2022-03-07 23:03:58 +0100

    deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,93 +1,20 @@
 <template>
   <h1>GOIMAGINARY</h1>
   <Particles
-    id="tsparticles"
-    :particlesInit="particlesInit"
-    :particlesLoaded="particlesLoaded"
-    :options="{
-        background: {
-            color: {
-                value: '#0d47a1'
-            }
-        },
-        fpsLimit: 120,
-        interactivity: {
-            events: {
-                onClick: {
-                    enable: true,
-                    mode: 'push'
-                },
-                onHover: {
-                    enable: true,
-                    mode: 'repulse'
-                },
-                resize: true
-            },
-            modes: {
-                bubble: {
-                    distance: 400,
-                    duration: 2,
-                    opacity: 0.8,
-                    size: 40
-                },
-                push: {
-                    quantity: 4
-                },
-                repulse: {
-                    distance: 200,
-                    duration: 0.4
-                }
-            }
-        },
-        particles: {
-            color: {
-                value: '#ffffff'
-            },
-            links: {
-                color: '#ffffff',
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1
-            },
-            collisions: {
-                enable: true
-            },
-            move: {
-                direction: 'none',
-                enable: true,
-                outMode: 'bounce',
-                random: false,
-                speed: 6,
-                straight: false
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 800
-                },
-                value: 80
-            },
-            opacity: {
-                value: 0.5
-            },
-            shape: {
-                type: 'circle'
-            },
-            size: {
-                random: true,
-                value: 5
-            }
-        },
-        detectRetina: true
-    }"
-  />
+      id="tsparticles"
+      :options="options"
+    />
 
   <p>just GO into the IMAGINARY world</p>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import options from "../options.js";
+
+let themeableContainer;
+
+
 
 </script>
 
